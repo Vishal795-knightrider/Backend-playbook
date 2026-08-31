a4acc2a053ef464e8e23091222abac6637e0f051
Vishal795-knightrider <vk3293801@gmail.com>
2026-08-31 10:41:17 +0530

    express server

--- express/server.js
+++ express/server.js
@@ -1,2 +1,18 @@
-const express=reuire('express')
-const app=express()
+const express = require('express')     // importing module 
+const app = express()  
+
+app.get('/', (req, res) => {
+    res.send("Home page it is")
+})
+
+app.get('/about', (req, res) => {
+    res.send("About sec")
+})
+
+app.post('/users', (req, res) => {
+    res.send("creation of user")
+})
+
+app.listen(3000, () => {
+    console.log("Server is ok") 
+})
