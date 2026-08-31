73c12a0e68b5e0588777ec7e1fc94129bbd51693
Vishal795-knightrider <vk3293801@gmail.com>
2026-08-31 12:29:33 +0530

    express get, post ,delete

--- express/server.js
+++ express/server.js
@@ -1,18 +1,61 @@
 const express = require('express')     // importing module 
 const app = express()  
+app.use(express.json())        //middleware
 
-app.get('/', (req, res) => {
-    res.send("Home page it is")
+let students=[
+{
+    id:1,
+    name: "vishal",
+    course: "Btech"
+},
+{
+    id:2,
+    name: "rohan",
+    course: "bca"
+}
+];
+
+app.get("/students", (req, res) => {
+    res.json(students)
 })
 
-app.get('/about', (req, res) => {
-    res.send("About sec")
+app.get("/students/:id",(req,res)=>{
+    const id=parseInt(req.params.id)
+    const stud=students.find(student=>student.id===id)
+    if(!stud){
+        res.status(404).json({
+            message:"not found"
+        })
+    }
+    res.json(stud);
 })
 
-app.post('/users', (req, res) => {
-    res.send("creation of user")
-})
+// app.post("/students/:id",(req,res)=>{
+//     const newStudent={
+//         id:students.length+1,
+//         name:req.body.name,
+//         course:req.body.course
+//     }
+//     students.push(newStudent)
 
-app.listen(3000, () => {
-    console.log("Server is ok") 
+//     res.status(201).json({
+//         message:"student insert sucess",
+//         student:newStudent
+//     })
+// })
+
+// app.delete("/students/:id",(req,res)=>{
+//     const id=parseInt(req.params.id)
+//     const idx=students.findIndex(student=>student.id===id)
+
+//     if(idx==-1){
+//         res.status(404).json({
+//             message:"not found"
+//         })
+//     }
+//     students.splice(idx,1)
+// })
+
+app.listen(3000,()=>{
+    console.log("okkk");
 })
