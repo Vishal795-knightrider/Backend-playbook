0c4e1f336df45382e74102f2718e38792debaf1c
Vishal795-knightrider <vk3293801@gmail.com>
2026-09-01 09:55:16 +0530

    put and patch

--- express/server.js
+++ express/server.js
@@ -55,10 +55,37 @@ app.delete("/students/:id",(req,res)=>{
     students.splice(idx,1)
     res.json({
         message:"deleted succes",
-        id:idx
+        student:students
     })
 })
 
+app.put("/students/:id",(req,res)=>{
+    const id=parseInt(req.params.id)
+    const studnt=students.find(student=>student.id===id)
+
+    if(!studnt){
+        res.status(404).json({
+            message:"not found"
+        })
+    }
+    studnt.name=req.body.name
+    studnt.course=req.body.name
+    res.json(studnt)
+})
+
+app.patch("/students/:id",(req,res)=>{
+    const id=parseInt(req.params.id)
+    const stud=students.find(student=>student.id===id)
+    if(!stud){
+        res.status(404).json({
+            mesage:"not foudn"
+        })
+    }
+    if(req.body.name!==undefined) stud.name=req.body.name
+    if(req.body.course!==undefined) stud.course=req.body.course
+
+    res.json(stud)
+})
 
 app.listen(3000,()=>{
     console.log("okkk");
